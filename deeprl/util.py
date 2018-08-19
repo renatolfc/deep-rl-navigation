8f1b992f3c4882bf6998a5fe4abc6ace1af0ae3d
Renato L. F. Cunha <renatocunha@acm.org>
2018-08-19 20:44:35 -0300

    code: fix bugs in utility code

--- deeprl/util.py
+++ deeprl/util.py
@@ -7,6 +7,8 @@ import logging
 import zipfile
 import platform
 
+from unityagents import UnityEnvironment
+
 try:
     from IPython.display import Markdown, display
 except Exception:
@@ -19,7 +21,6 @@ def build_environment(path):
     directory = os.path.dirname(path)
     if not os.path.exists(directory):
         zipped = directory + '.zip'
-        print(zipped)
         if os.path.exists(zipped):
             with zipfile.PyZipFile(zipped) as zfp:
                 logging.info('Extracting environment...')
@@ -33,30 +34,19 @@ def get_executable_path():
     parent = os.path.dirname(here)
     if platform.system() == 'Linux':
         if '64' in platform.architecture()[0]:
-            env = build_environment(
-                os.path.join(here, 'Banana_Linux', 'Banana.x86_64')
-            )
+            return os.path.join(parent, 'Banana_Linux', 'Banana.x86_64')
         else:
-            env = build_environment(
-                os.path.join(here, 'Banana_Linux', 'Banana.x86')
-            )
+            return os.path.join(parent, 'Banana_Linux', 'Banana.x86')
     elif platform.system() == 'Darwin':
-        env = build_environment(
-            os.path.join(here, 'Banana.app')
-        )
+            return os.path.join(parent, 'Banana.app')
     elif platform.system() == 'Windows':
         if '64' in platform.architecture()[0]:
-            env = build_environment(
-                os.path.join(here, 'Banana_Windows_x86_64', 'Banana.exe')
-            )
+            return os.path.join(parent, 'Banana_Windows_x86_64', 'Banana.exe')
         else:
-            env = build_environment(
-                os.path.join(here, 'Banana_Windows_x86', 'Banana.exe')
-            )
+            return os.path.join(parent, 'Banana_Windows_x86', 'Banana.exe')
     else:
         logging.error('Unsupported platform!')
         raise ValueError('Unsupported platform')
-    return env
 
 def print_source(obj):
     source = inspect.getsource(obj)
