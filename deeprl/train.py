c81bf027c272466171ac92e16c4cee02f8e921a1
Renato L. F. Cunha <renatocunha@acm.org>
2018-08-28 06:14:54 -0300

    train: add support for training in the visual problem

--- deeprl/train.py
+++ deeprl/train.py
@@ -14,16 +14,31 @@ from .util import load_environment
 from .agent import Agent
 
 
+def rgb2gray(img):
+    img = img.squeeze()
+    if len(img.shape) == 3:
+        # return img.dot([0.299, 0.587, 0.114]).reshape(
+        #     1, img.shape[0], img.shape[1], 1
+        # )
+        return (img ** 2).dot(
+            [0.299, 0.587, 0.114]
+        ).reshape(1, img.shape[0], img.shape[1], 1)
+    else:
+        raise ValueError('Image in some color space not known')
+
+
 def get_state(env_info, use_visual):
     if use_visual:
         state = env_info.visual_observations[0]
+        state = rgb2gray(state)
     else:
         state = env_info.vector_observations[0]
     return state
 
 
-def dqn(env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.001,
-        eps_decay=0.995, solution_threshold=13.0, checkpointfn='checkpoint.pth'):
+def dqn(env, n_episodes=1001, max_t=1000, eps_start=1.0, eps_end=0.001,
+        eps_decay=0.995, solution_threshold=13.0, checkpointfn='checkpoint.pth',
+        load_checkpoint=False):
     """Function that uses Deep Q Networks to learn environments.
 
     Parameters
@@ -49,10 +64,13 @@ def dqn(env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.001,
 
     if state_size == 0:
         use_visual = True
-        state = env_info.visual_observations[0]
-        state_size = state.shape
+        state = get_state(env_info, use_visual)
+        state_size = (1, state.shape[1], state.shape[2], state.shape[3])
 
-    agent = Agent(state_size, action_size, 123, use_visual)
+    if load_checkpoint:
+        agent = Agent.load(checkpointfn, use_visual)
+    else:
+        agent = Agent(state_size, action_size, 123, use_visual)
 
     scores = []  # All episodes seen over training
     scores_window = deque(maxlen=100)  # last 100 scores
@@ -78,13 +96,17 @@ def dqn(env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.001,
         eps = max(eps_end, eps_decay*eps) # decrease epsilon
         logging.debug(
             'Episode {}\tAverage Score: {:.2f}'
-            .format(i_episode + 1, np.mean(scores_window))
+            .format(i_episode, np.mean(scores_window))
         )
-        if i_episode % 100 == 0:
+        if (i_episode + 1) % 100 == 0:
             logging.info(
                 'Episode {}\tAverage Score: {:.2f}'
-                .format(i_episode + 1, np.mean(scores_window))
+                .format(i_episode, np.mean(scores_window))
             )
+            logging.info(
+                'Saving checkpoint file...'
+            )
+            agent.save(checkpointfn)
         if np.mean(scores_window) >= solution_threshold:
             logging.info(
                 'Environment solved in {:d} episodes!'
@@ -95,18 +117,22 @@ def dqn(env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.001,
             )
             agent.save(checkpointfn)
             break
+
     return agent, scores, i_episode - 99
 
 def main():
     parser = argparse.ArgumentParser(description='Trains a learning agent')
     parser.add_argument('--checkpoint', dest='checkpoint', action='store',
                         default='checkpoint.pth')
+    parser.add_argument('--load-checkpoint', dest='load_chkpt', action='store_true',
+                        default=False)
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
 
     env = load_environment()
-    dqn(env, checkpointfn=args.checkpoint)
+    dqn(env, eps_decay=0.9975, n_episodes=50002,
+        checkpointfn=args.checkpoint, load_checkpoint=args.load_chkpt)
 
 if __name__ == '__main__':
     main()
