45ce23590a19ddbb30a54730d3c3cc21100038e7
Renato L. F. Cunha <renatocunha@acm.org>
2018-08-28 21:43:51 -0300

    train: add support for reloading the process to prevent memory leaks

--- deeprl/train.py
+++ deeprl/train.py
@@ -3,6 +3,8 @@
 
 from __future__ import print_function
 
+import os
+import sys
 import logging
 import argparse
 from collections import deque
@@ -30,7 +32,6 @@ def rgb2gray(img):
 def get_state(env_info, use_visual):
     if use_visual:
         state = env_info.visual_observations[0]
-        state = rgb2gray(state)
     else:
         state = env_info.vector_observations[0]
     return state
@@ -38,7 +39,7 @@ def get_state(env_info, use_visual):
 
 def dqn(env, n_episodes=1001, max_t=1000, eps_start=1.0, eps_end=0.001,
         eps_decay=0.995, solution_threshold=13.0, checkpointfn='checkpoint.pth',
-        load_checkpoint=False):
+        load_checkpoint=False, reload_every=None):
     """Function that uses Deep Q Networks to learn environments.
 
     Parameters
@@ -68,14 +69,19 @@ def dqn(env, n_episodes=1001, max_t=1000, eps_start=1.0, eps_end=0.001,
         state_size = (1, state.shape[1], state.shape[2], state.shape[3])
 
     if load_checkpoint:
-        agent = Agent.load(checkpointfn, use_visual)
+        try:
+            agent = Agent.load(checkpointfn, use_visual)
+        except Exception:
+            logging.exception('Failed to load checkpoint. Ignoring...')
+            agent = Agent(state_size, action_size, 123, use_visual)
     else:
         agent = Agent(state_size, action_size, 123, use_visual)
 
-    scores = []  # All episodes seen over training
-    scores_window = deque(maxlen=100)  # last 100 scores
-    eps = eps_start
-    for i_episode in range(n_episodes):
+    if agent.episode:
+        eps = (eps_start * eps_decay) ** agent.episode
+    else:
+        eps = eps_start
+    for i_episode in range(agent.episode, n_episodes):
         env_info = env.reset(train_mode=True)[brain_name]
         state = get_state(env_info, use_visual)
 
@@ -91,23 +97,24 @@ def dqn(env, n_episodes=1001, max_t=1000, eps_start=1.0, eps_end=0.001,
             score += reward
             if done:
                 break
-        scores_window.append(score)       # save most recent score
-        scores.append(score)              # save most recent score
+        agent.scores.append(score)
         eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        agent.episode += 1
+
         logging.debug(
-            'Episode {}\tAverage Score: {:.2f}'
-            .format(i_episode, np.mean(scores_window))
+            'Episode {}\tAverage Score: {:.2f}\tCurrent Score: {:.2f}\tEpsilon: {:.4f}'
+            .format(i_episode, np.mean(agent.scores[-100:]), score, eps)
         )
         if (i_episode + 1) % 100 == 0:
             logging.info(
                 'Episode {}\tAverage Score: {:.2f}'
-                .format(i_episode, np.mean(scores_window))
+                .format(i_episode, np.mean(agent.scores[-100:]))
             )
             logging.info(
                 'Saving checkpoint file...'
             )
             agent.save(checkpointfn)
-        if np.mean(scores_window) >= solution_threshold:
+        if np.mean(agent.scores[-100:]) >= solution_threshold:
             logging.info(
                 'Environment solved in {:d} episodes!'
                 .format(i_episode - 99)
@@ -117,8 +124,19 @@ def dqn(env, n_episodes=1001, max_t=1000, eps_start=1.0, eps_end=0.001,
             )
             agent.save(checkpointfn)
             break
+        if reload_every and i_episode and (i_episode + 1) % reload_every == 0:
+            env.close()
+            reload_process()
 
-    return agent, scores, i_episode - 99
+    return agent, i_episode - 99
+
+
+def reload_process():
+    if '--load-checkpoint' not in sys.argv:
+        sys.argv.append('--load-checkpoint')
+    os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    os.execv('/proc/self/exe', 'python -m deeprl.train'.split() + sys.argv[1:])
+
 
 def main():
     parser = argparse.ArgumentParser(description='Trains a learning agent')
@@ -128,10 +146,10 @@ def main():
                         default=False)
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO)
+    logging.getLogger().setLevel(logging.DEBUG)
 
     env = load_environment()
-    dqn(env, eps_decay=0.9975, n_episodes=50002,
+    dqn(env, eps_decay=0.999, n_episodes=100000, reload_every=500,
         checkpointfn=args.checkpoint, load_checkpoint=args.load_chkpt)
 
 if __name__ == '__main__':
